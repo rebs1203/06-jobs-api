9bf7ea0e8af183b2e24d05c7c2810131a4e3ec84
rebecavg <rebecavgallo@hotmail.com>
2023-12-12 16:21:26 -0500

    finished exercise

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 require('express-async-errors');
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 const express = require('express');
 const app = express();
 
@@ -17,8 +21,15 @@ const groceriesRouter = require('./routes/groceries')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 //routes
 
--- controllers/groceries.js
+++ controllers/groceries.js
@@ -3,12 +3,26 @@ const {BadRequestError, NotFoundError} = require('../errors')
 const Groceries = require('../models/Groceries')
 
 const getAllGroceries = async (req, res) => {
-    res.send('foood')
+    const products = await Groceries.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({products})
 }
 
 const getProduct = async (req, res) => {
-    res.send('product')
+    const {user:{userId}, params:{id:productId}} = req
+
+    const product = await Groceries.findOne({
+        _id: productId,
+        createdBy: userId
+    })
+
+    if (!product) {
+        throw new NotFoundError(`No product with id: ${productId}`)
+    }
+
+    res.status(StatusCodes.OK).json({product})
 }
+
+
 
 const createProduct = async (req, res) => {
     req.body.createdBy = req.user.userId
@@ -17,11 +31,30 @@ const createProduct = async (req, res) => {
 }
 
 const updateProduct = async (req, res) => {
-    res.send('update product')
+    const {user:{userId}, params:{id:productId}, body:{name}} = req
+
+    if (name === '') {
+        throw new BadRequestError('Name field cannot be empty')
+    }
+
+    const product = await Groceries.findByIdAndUpdate({_id:productId, createdBy: userId}, req.body, {new:true, runValidators:true})
+
+    if (!product) {
+        throw new NotFoundError(`No product with id: ${productId}`)
+    }
+
+    res.status(StatusCodes.OK).json({product})
 }
 
 const deleteProduct = async (req, res) => {
-    res.send('delete product')
+    const {user: {userId}, params: {id: productId}} = req
+
+    const product = await Groceries.findByIdAndRemove({_id:productId, createdBy:userId})
+
+    if(!product) {
+        throw new NotFoundError(`No product with id: ${productId}`)
+    }
+    res.status(StatusCodes.OK)
 }
 
 module.exports = {
